fa76befb33b960263c3d9b56b53193691a5162d8
Seulgigi <2021112414@dgu.ac.kr>
2023-06-26 15:34:39 +0900

    feat: EDD 스케줄링 알고리즘

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ class Job:
     def __init__(self, _id: int):
         self.ID = _id  # instance variable unique to each instance
         self.complete = False
-        self.start = -1
-        self.end = -1
+        self.start = -1  # 작업의 시작 시간
+        self.end = -1  # 작업이 끝나는 시간
         self.assignedMch = -1
 
     def __repr__(self):
@@ -20,8 +20,8 @@ class Machine:
 
     def __init__(self, _id: int):
         self.ID = _id  # instance variable unique to each instance
-        self.available = 0
-        self.assigned = []
+        self.available = 0  # 작업이 가능한 시점
+        self.assigned = []  # machine에 할당된 job list
 
     def __repr__(self):
         return 'Machine ' + str(self.ID)
@@ -35,11 +35,11 @@ class Instance:
         self.numMch = len(mchs)
         self.job_list = jobs
         self.machine_list = mchs
-        self.ptime = ptime
-        self.setup = setups
+        self.ptime = ptime  # 프로세스 타임
+        self.setup = setups  # 셋업 타임
 
     def getPTime(self, job: Job, machine: Machine):
-        pass
+        return self.ptime[machine.ID][job.ID]  # add time return code
 
     def getSetup(self, job_i: Job, job_j: Job, machine: Machine):
         return self.setup[machine.ID][job_i.ID][job_j.ID]
@@ -65,11 +65,39 @@ def generate_prob(numJob, numMch) -> Instance:
 
 
 def scheduling(prob:Instance, alg:str) -> Schedule:
-    if alg == 'EDD':
-        pass
+    if alg == 'EDD':  # 납기일이 빠른 순서대로 스케줄링
+        sorted_job = sorted(prob.job_list, key=lambda j: j.end)
+        # 작업 종료 시간(납기일)을 기준으로 오름차순 정렬
+
+        for j in sorted_job:
+            selected_machine = min(prob.machine_list, key=lambda m: m.available)
+            # machine의 이전 작업이 끝나는 시간, 즉 end가 빠른 machine을 선택
+
+            if len(selected_machine.assigned) == 0:
+                j.start = selected_machine.available
+            else:
+                j.start = selected_machine.available + prob.getSetup(selected_machine.assigned[-1], j, selected_machine)
+                # selected_machine에 이전 작업에서 현재 작업으로의 setuptime을 더함
+
+            j.end = j.start + prob.getPTime(j, selected_machine)
+            # 작업 종료 시간 설정
+
+            j.assignedMch = selected_machine.ID
+            # job 지정된 machine의 번호 저장
+
+            selected_machine.available = j.end + prob.getSetup(j, j, selected_machine)
+            # 기계의 다음 작업 시작 시간
+
+            selected_machine.assigned.append(j)
+            # 기계에 할당된 작업 추가
+
+        for m in prob.machine_list:
+            print(m.assigned)
+
+
     print('Scheduling is done.')
 
 
 if __name__ == '__main__':
     test_instance = generate_prob(numJob=10, numMch=5)
-    schedule = scheduling(test_instance)
+    schedule = scheduling(test_instance, 'EDD')
